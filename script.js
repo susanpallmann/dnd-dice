19ad1987b90d13917ac37e46c1dfadfd88e27802
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-10-18 14:19:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables to store min and max values of dice rolls, as well as their names
 var d4 = [1, 4, "d4"];
 var d6 = [1, 6, "d6"];
 var d8 = [1, 8, "d8"];
@@ -5,47 +6,84 @@ var d10 = [1, 10, "d10"];
 var d12 = [1, 12, "d12"];
 var d20 = [1, 20, "d20"];
 var dp = [00, 10, "dp"];
+
+// Stores dice we are going to be rolling
 var dice = [];
 
+
+// Function to return a random number from min and max values
 function randomNumber(min, max) {
-  var random = Math.floor(Math.random() * max) + min;
-  return random;
+    var random = Math.floor(Math.random() * max) + min;
+    return random;
 }
 
+// Given an object representing a die (called LOL), adds die to our dice array
 function AddDice(LOL) {
-  dice.push(LOL);
+    dice.push(LOL);
 }
 
+// Removes all dice from our dice array (no need to remove individually)
 function RemoveDice() {
-  dice = [];
+    dice = [];
 }
 
+// Rolls the dice currently in the dice array
 function rollDice() {
-  var visualDice = [];
-  $('.visual-dice').each(function() {
+
+    // Creates an array to store the visual representation of our dice
+    var visualDice = [];
+
+    // For each die item visible in the waiting area
+    $('.visual-dice').each(function() {
+
+    // Add this die to the array
     visualDice.push($(this));
-  });
-  for (var i = 0; i < dice.length; i++) {
-    var rollNumber = randomNumber(dice[i][0], dice[i][1]);
-    var diceName = dice[i][2];
-    for (var j = 0; j < visualDice.length; j++) {
-      if (visualDice[j].hasClass(diceName)) {
-        console.log('test');
-        visualDice[j].text(rollNumber);
-        visualDice.splice(j, 1);
-        break;
-      }
+    });
+
+    // For each dice stored in the dice array (note: not the visual dice array)
+    for (var i = 0; i < dice.length; i++) {
+
+        // Get random number appropriate to that die type's min and max values
+        var rollNumber = randomNumber(dice[i][0], dice[i][1]);
+
+        // Gets the name of the die so we can match it to a DOM element
+        var diceName = dice[i][2];
+
+        // For each visual die in our array
+        for (var j = 0; j < visualDice.length; j++) {
+
+            // If the class matches the die we are currently iterated on from the dice array
+            if (visualDice[j].hasClass(diceName)) {
+
+                // Set the text to roll number (we'll replace this with cooler functionality later)
+                visualDice[j].text(rollNumber);
+
+                // Remove from the visual dice array, we don't want to update this one again
+                visualDice.splice(j, 1);
+
+                // Stop looping through visual dice when we found a match for our current dice die
+                break;
+            }
+        }
     }
-  }
-  RemoveDice();
+
+    // Clears dice array when we have iterated through all of the dice
+    RemoveDice();
+
+    // TODO: We'll want to move dice from waiting to rolling
+    // TODO: We'll need to remove them visually after rolling
 }
 
+// Empties waiting area
 function clearWaitingArea() {
-  $('#waiting').empty();
+    $('#waiting').empty();
 }
 
+// Displays dice in the waiting area, LMAO in this case represents the dice array
 function displayDice(LMAO) {
-clearWaitingArea();
+  
+  
+    clearWaitingArea();
     for (var i = 0; i < LMAO.length; i++) {
         $('#waiting').append('<span class="visual-dice ' + LMAO[i][2] + '">' + LMAO[i][2] + '</span>');
     }
@@ -61,6 +99,8 @@ $('document').ready(function () {
         rollDice();
     });
 
+    // Buttons handling because unfortunately we need to pass a direct variable into AddDice
+    // (Objects and classes felt a little advanced for his first program)
     $('#buttons').on('click', '.button', function() {
         if ($(this).is("#d4")) {
             AddDice(d4);
